6715a38452d2f93f05b490c15cf35441529a4a0b
Willian Nascimento <45379088+d4sein@users.noreply.github.com>
2020-08-17 18:05:41 -0300

    Added doc for replies usage

--- src/replies.config.ts
+++ src/replies.config.ts
@@ -4,11 +4,18 @@ const reportIssue = 'Please report the issue at https://github.com/d4sein/Bumble
 
 export const errors = {
   noBotPrefix: 'Oh, no. My prefix didn\'t get set properly, sorry :zany_face:.',
+  // Use this whenever there's an error with a command, ex:
+  // Keys not set correctly in the Map parameters,
+  // Missing necessary information to run a command.
   badCommandConfig: `Seems like a bad developer worked on this one :zany_face:.\n${reportIssue}`,
+  // Use this whenever there's an error that can't be explained by a
+  // poorly writen command or incorrect usage of a command by the user.
   unexpected: 'Something happend, it doesn\'t seem to be a problem with neither me nor you :thinking:.'
 }
 
 export const responses = {
+  // Use this whenever there's an error caused by
+  // incorrect usage of a command by the user.
   fnIncorrectUsage: (usage: string): string => {
     return `You're not doing it right, look: \`${usage}\``
   }
